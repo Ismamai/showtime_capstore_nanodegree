567c7f1a4421f328611cb54f43efdcaec673b1bd
iblesa <isma.work@gmail.com>
2018-08-05 21:14:33 +0100

    Added retrofit to retrieve data

---
+++ app/src/main/java/com/iblesa/api/ApiService.java
@@ -0,0 +1,14 @@
+package com.iblesa.api;
+
+import com.iblesa.api.models.EventResponse;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiService {
+    //https://app.ticketmaster.com/discovery/v2/events.json?apikey=key_here
+    @GET("events.json")
+    Call<EventResponse> listEvents(@Query("apikey") String apikey);
+}
