63b579eb3d20ee2d9ea0f4b375e458dc095f16b6
iblesa <isma.work@gmail.com>
2018-08-11 10:59:58 +0100

    Added ContentURI

--- app/src/main/java/com/iblesa/api/data/EventContract.java
+++ app/src/main/java/com/iblesa/api/data/EventContract.java
@@ -1,13 +1,25 @@
 package com.iblesa.api.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class EventContract {
+
+    public static final String AUTHORITY = "com.iblesa.showtime";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://"+AUTHORITY);
+
+    public static final String PATH_EVENTS = "events";
+
     // Prevent accidentally instantiating of contract class
     private EventContract() {
     }
 
     public static class EventEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_EVENTS)
+                .build();
+
         public static final String TABLE_NAME = "events";
         public static final String COLUMN_EVENT_NAME= "name";
         public static final String COLUMN_EVENT_DATE= "date";
