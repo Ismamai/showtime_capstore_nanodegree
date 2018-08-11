c7e3579fa3a83a7e42b235a49ceaf92e3eaffef9
iblesa <isma.work@gmail.com>
2018-08-11 10:14:34 +0100

    Created base EventContentProvider Added Provider to AndroidManifest

---
+++ app/src/main/java/com/iblesa/api/data/EventContentProvider.java
@@ -0,0 +1,49 @@
+package com.iblesa.api.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class EventContentProvider extends ContentProvider {
+    private EventDbHelper mEventDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        Context context = getContext();
+        mEventDbHelper = new EventDbHelper(context);
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
