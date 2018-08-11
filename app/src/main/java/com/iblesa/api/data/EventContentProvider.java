8ed5971f2eb62a176ff686b77fa4a335c6dbe269
iblesa <isma.work@gmail.com>
2018-08-11 11:07:21 +0100

    Building uriMatcher

--- app/src/main/java/com/iblesa/api/data/EventContentProvider.java
+++ app/src/main/java/com/iblesa/api/data/EventContentProvider.java
@@ -3,13 +3,29 @@ package com.iblesa.api.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.iblesa.api.models.Event;
+
 public class EventContentProvider extends ContentProvider {
     private EventDbHelper mEventDbHelper;
+
+    private static final int EVENTS = 100;
+
+    public static final UriMatcher sUriMatcher = buildUtiMatcher();
+
+    public static UriMatcher buildUtiMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        //List all events
+        uriMatcher.addURI(EventContract.AUTHORITY, EventContract.PATH_EVENTS, EVENTS);
+
+        return uriMatcher;
+    }
 
     @Override
     public boolean onCreate() {
