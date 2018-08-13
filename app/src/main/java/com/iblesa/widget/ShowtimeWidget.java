fc9eb1d0fb0d1046910fb054fc9558c12dd321b3
iblesa <isma.work@gmail.com>
2018-08-13 21:20:26 +0100

    Nice working adding the widget, however you seemed to have missed to call the onUpdate() method of super class without which the widget is not loading. Please add the call to super as follows:
    super.onUpdate(context, appWidgetManager, appWidgetIds);
    This will make the widget work flawlessly :smiley:

--- app/src/main/java/com/iblesa/widget/ShowtimeWidget.java
+++ app/src/main/java/com/iblesa/widget/ShowtimeWidget.java
@@ -40,6 +40,7 @@ public class ShowtimeWidget extends AppWidgetProvider {
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager,appWidgetIds);
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
